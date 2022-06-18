2370aae3fd8a02b8f4586b7f3c1503d3017aa9ec
Vidyasagar5566 <85301020+Vidyasagar5566@users.noreply.github.com>
2022-06-18 09:38:35 +0530

    Delete InstaBook directory

--- InstaBook/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class user_posts(models.Model):
-    u_name = models.CharField(max_length = 100)
-    U_main_pic = models.ImageField(upload_to = 'pics')
-    caption = models.TextField()
-    comments = models.TextField(default="@")
